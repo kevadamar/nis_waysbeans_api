79a63948378a5ef490dc08cf468dfe1363eede66
keva damar galih <kevadamarg@gmail.com>
2021-07-29 06:40:58 +0700

    update middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const { signin } = require('../controllers/authController');
 const { getAllUsers } = require('../controllers/userController');
-const { authMiddleware } = require('../middleware/authMiddleware');
+const { authMiddleware, adminAccessMiddleware } = require('../middleware/authMiddleware');
 
 const router = require('express').Router();
 
 // Users route
-router.get('/users', authMiddleware, getAllUsers);
+router.get('/users', authMiddleware, adminAccessMiddleware, getAllUsers);
 
 // AUTH
 router.post('/login', signin);
