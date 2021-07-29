456458e859ae8c5ea65d132cf4670443055b86a7
keva damar galih <kevadamarg@gmail.com>
2021-07-29 12:37:03 +0700

    add Landing page

---
+++ src/controllers/productController.js
@@ -0,0 +1,29 @@
+const { Product } = require('../../models');
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const { rows, count } = await Product.findAndCountAll({
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
+    const resultProducts = JSON.parse(JSON.stringify(rows));
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully!',
+      counData: count,
+      data: resultProducts,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: 500,
+      message: 'Internal Server Error',
+      error,
+    });
+  }
+};
+
+
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,19 @@
-const { signin, signup } = require('../controllers/authController');
-const { getAllUsers } = require('../controllers/userController');
-const { authMiddleware, adminAccessMiddleware } = require('../middleware/authMiddleware');
-
 const router = require('express').Router();
+
+const { signin, signup } = require('../controllers/authController');
+const { getAllProducts, getProduct } = require('../controllers/productController');
+const { getAllUsers } = require('../controllers/userController');
+const {
+  authMiddleware,
+  adminAccessMiddleware,
+} = require('../middleware/authMiddleware');
+
 
 // Users route
 router.get('/users', authMiddleware, adminAccessMiddleware, getAllUsers);
+
+// Product route
+router.get('/products', getAllProducts);
 
 // AUTH
 router.post('/login', signin);
